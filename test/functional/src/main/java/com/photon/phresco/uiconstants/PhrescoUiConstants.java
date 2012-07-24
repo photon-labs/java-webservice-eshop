c487500dc9599474bedeff5709c7e368d0b6b748
vairamuthu-m <vairamuthu.mottaiyan@photoninfotech.net>
2012-07-24 19:22:03 +0530

    Artf504998: Javawebservice functional test cases

--- test/functional/src/main/java/com/photon/phresco/uiconstants/PhrescoUiConstants.java
+++ test/functional/src/main/java/com/photon/phresco/uiconstants/PhrescoUiConstants.java
@@ -1,6 +1,6 @@
 /*
  * ###
- * PHR_JavaWebService
+  * PHR_JavaWebService
  * %%
  * Copyright (C) 1999 - 2012 Photon Infotech Inc.
  * %%
@@ -19,24 +19,25 @@
  */
 package com.photon.phresco.uiconstants;
 
-import com.photon.phresco.uiconstants.TestProperties;
 import java.lang.reflect.Field;
 
+
 public class PhrescoUiConstants {
+	
+	private ReadXMLFile readXml;
 
-	public String PROTOCOL = "server.protocol";
-	public String SERVER_PORT = "server.port";
-	public String CONTEXT = "server.context";
-	public String HOST = "server.host";
-	public int PORT = 4444;
-	public String BROWSER = "browser.name";
-	public String SPEED = "PHOTON_PHRESCO_SPEED";
 
-	// ***************LOGINPAGE*****************
-    public String TEXTCAPTURED="PHOTON_PHRESCO_TEXTCAPTURED";
-	// ***************LOGINPAGE*****************
+	public String BROWSER = "Browser";
+    public String HOST = "host";
+	public String PORT = "port";
+	public String PROTOCOL = "protocol";
+	public String CONTEXT = "context";
 
-	private static PhrescoUiConstants instance;
+	//public String ELEMENT = "stringCapturehelloworldUrl";
+
+	
+
+ 	private static PhrescoUiConstants instance;
 
 	public static PhrescoUiConstants getInstance() {
 		if (instance == null)
@@ -45,23 +46,24 @@ public class PhrescoUiConstants {
 	}
 
 	public PhrescoUiConstants() {
-		try {
-			TestProperties localTestProperties = new TestProperties();
-			localTestProperties.loadPhrescoConfigProperties();
-			Field[] arrayOfField1 = super.getClass().getFields();
-			Field[] arrayOfField2 = arrayOfField1;
-			int i = arrayOfField2.length;
-			for (int j = 0; j < i; ++j) {
-				Field localField = arrayOfField2[j];
-				Object localObject = localField.get(this);
-				if (localObject instanceof String)
-					localField.set(this,
-							localTestProperties.getValue((String) localObject));
+	
+			try {
+			    readXml = new ReadXMLFile();
+				Field[] arrayOfField1 = super.getClass().getFields();
+				Field[] arrayOfField2 = arrayOfField1;
+				int i = arrayOfField2.length;
+				for (int j = 0; j < i; ++j) {
+					Field localField = arrayOfField2[j];
+					Object localObject = localField.get(this);
+					if (localObject instanceof String)
+						localField
+								.set(this, readXml.getValue((String) localObject));
+					//System.out.println("*********Reading Data from UIConstants**********");
+				}
+			} catch (Exception localException) {
+				throw new RuntimeException("Loading "
+						+ super.getClass().getSimpleName() + " failed",
+						localException);
 			}
-		} catch (Exception localException) {
-			throw new RuntimeException("Loading "
-					+ super.getClass().getSimpleName() + " failed",
-					localException);
 		}
 	}
-}
